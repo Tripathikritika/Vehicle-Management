46c29077946bd581959448e0206c8bef4b17eb04
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-11-22 03:32:07 +0530

    Sorting and filtering via Backend

--- Backend/controllers/vehicle_controller.js
+++ Backend/controllers/vehicle_controller.js
@@ -1,15 +1,49 @@
 const Vehicles = require('../models/vehicle_model')
 
-const getVehicles = ( req , res ) => {
-    Vehicles.find()
-            .then((item) => res.status(200).json(item))
-            .catch((err) => res.status(400).json( { message : "Failed To Find from BackEnd"}))
+const getVehicles =async ( req , res ) => {
+    const page = Number(req.query.page);
+    const limit = Number(req.query.limit);
+    const sortArray = req.query.sort === "Asc" ? 1 : req.query.sort === 'Desc' ? -1 : 0
+    const filterArray = req.query.filters === 'Car' ? 'Car' : req.query.filters === 'Bus' ? 'Bus' : req.query.filters === 'Auto Ricksaw' ? "Auto Ricksaw" : req.query.filters ==="Bike" ? "Bike" : null
+    const filteredData = {operators:req.params.email}
+    if(filterArray){
+        filteredData['type'] = filterArray
+    }
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+    results.totalCount = await Vehicles.countDocuments().exec()
+
+    if (endIndex < (await Vehicles.countDocuments().exec())) {
+        results.next = {
+            page: page + 1,
+            limit: limit,
+        };
+    }
+
+    if (startIndex > 0) {
+        results.prev = {
+            page: page - 1,
+            limit: limit,
+        };
+    }
+
+    try{
+        results.current = await  Vehicles.find(filteredData).sort({no_of_passengers : sortArray}).limit(limit).skip(startIndex).exec()
+        res.json(results)
+    }
+    catch(e){
+        res.status(500).json( { message : e.jmessage})
+    }
+
 }
 
 const postVehicles = ( req , res ) => {
     // console.log(req.body)
-    const { registration_id, name , image , price , type ,no_of_passengers , vehicle_trips,operators} = req.body
-    const vehiclesList = new Vehicles({ registration_id,  name , image , price , type ,no_of_passengers , vehicle_trips,operators })
+    const registration_id = Vehicles.length + 1
+    const { name , image , price , type ,no_of_passengers , vehicle_trips,operators} = req.body
+    const vehiclesList = new Vehicles({name  , image ,price , type ,no_of_passengers , vehicle_trips,operators })
     vehiclesList
     .save()
     .then( () => res.json("Vehicles Added Successfully"))
